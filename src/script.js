91c68103102b52fcffca44184c7d33d7a287ee2c
Warren Brown <warrengbrn@gmail.com>
2025-07-17 13:59:54 -0400

    testing orthographic rotation performance

--- src/script.js
+++ src/script.js
@@ -2,42 +2,42 @@
 import * as d3 from "d3";
 
 //capture video
-var mediaRecorder;
+// var mediaRecorder;
 
- navigator.mediaDevices.getDisplayMedia({ video: true, preferCurrentTab: true })
-        .then(function(stream) {
+//  navigator.mediaDevices.getDisplayMedia({ video: true, preferCurrentTab: true })
+//         .then(function(stream) {
 
-            const options = {
-                videoBitsPerSecond: 500000000,
-            };
+//             const options = {
+//                 videoBitsPerSecond: 500000000,
+//             };
 
-            mediaRecorder = new MediaRecorder(stream, options);
+//             mediaRecorder = new MediaRecorder(stream, options);
 
-            let chunks = [];
+//             let chunks = [];
 
-            mediaRecorder.ondataavailable = (e) => {
-                if (e.data && e.data.size > 0) {
-                    chunks.push(e.data);
-                }
-            }
+//             mediaRecorder.ondataavailable = (e) => {
+//                 if (e.data && e.data.size > 0) {
+//                     chunks.push(e.data);
+//                 }
+//             }
 
-            mediaRecorder.onstop = function () {
-                const blob = new Blob(chunks, { type: 'image/png' });
-                const url = URL.createObjectURL(blob);
-                const a = document.createElement('a');
-                a.href = url;
-                a.download = 'canvas-recording.mp4';
-                if (window.confirm("Download video?")) {
-                    a.click();
-                }
-            }
+//             mediaRecorder.onstop = function () {
+//                 const blob = new Blob(chunks, { type: 'image/png' });
+//                 const url = URL.createObjectURL(blob);
+//                 const a = document.createElement('a');
+//                 a.href = url;
+//                 a.download = 'canvas-recording.mp4';
+//                 if (window.confirm("Download video?")) {
+//                     a.click();
+//                 }
+//             }
 
-            mediaRecorder.start();
+//             mediaRecorder.start();
 
-        })
-        .catch(function(err) {
-            console.log("Error accessing display media:", err);
-        });
+//         })
+//         .catch(function(err) {
+//             console.log("Error accessing display media:", err);
+//         });
 
 // testing screen recording frames to png
 // const start = async () => {
@@ -47,8 +47,8 @@ var mediaRecorder;
 // start();
 
 // svg dimensions
-const height = 2160;
-const width = 3840;
+const height = 500;
+const width = 940;
 
 // svg container
 const svg = d3.create("svg")
@@ -85,7 +85,7 @@ const dataPoints = async () => {
     commitments = await getCommmitments.json();
 
     // assign projection and path values
-    projection = d3.geoNaturalEarth1().fitSize([width, height], geoJson).rotate([-10, 0]);
+    projection = d3.geoOrthographic().fitSize([width, height], geoJson).rotate([-10, 0]);
     path = d3.geoPath().projection(projection);
 
     // calculate centroids for every country
@@ -124,71 +124,84 @@ dataPoints();
 var year = 1952;
 
 // testing rotation
-var rotation = [-10, 0];
+var rotation = [0,0];
+// d3.timer((elapsed) => {
+//     const rotate = projection.rotate();
+//     projection.rotate([
+//         rotate[0] - 1,
+//         rotate[1]
+//     ]);
+//     path = d3.geoPath().projection(projection);
+//     svg.selectAll("path")
+//         .attr("d", path);
+//         console.log(elapsed)
 
-// setInterval(() => {
-//     rotation[0] = rotation[0] + 1;
-//             svg.selectAll("path")
-//             .data(geoJson.features)
-//             .join("path")
-//             .attr("d", path)
-//             .attr("fill", "rgba(72, 153, 210, 1)")
-//             .attr("stroke", "black")
-//             .attr("stroke-width", 1);
-        
-        
-//         path = d3.geoPath().projection(projection.rotate(rotation));
-// }, 20);
+//     }, 1000);
 
 setInterval(() => {
-    if (year <= 2025) {
+    rotation[0] = rotation[0] - 1;
+            path = d3.geoPath().projection(projection.rotate(rotation));
 
-        // clear images from document
-        var previousFlags;
-        previousFlags = document.querySelectorAll('.flagImage');
-        for (var i = 0; i < previousFlags.length; i++) {
-            previousFlags[i].remove();
-        }
+            svg.selectAll("path")
+            .data(geoJson.features)
+            .join("path")
+            .attr("d", path)
+            .attr("fill", "rgba(72, 153, 210, 1)")
+            .attr("stroke", "black")
+            .attr("stroke-width", 1);
+        
+        
+}, 20);
+
+// setInterval(() => {
+//     if (year <= 2025) {
+
+//         // clear images from document
+//         var previousFlags;
+//         previousFlags = document.querySelectorAll('.flagImage');
+//         for (var i = 0; i < previousFlags.length; i++) {
+//             previousFlags[i].remove();
+//         }
 
 
-        for (var i = 0; i < commitments.length; i++) {
-            if (commitments[i].Year == year) {
-                console.log(commitments[i]);
+//         for (var i = 0; i < commitments.length; i++) {
+//             if (commitments[i].Year == year) {
+//                 console.log(commitments[i]);
 
-                // append the flag images to the map
-                for (var a = 0; a < centroids.length; a++) {
-                    if (commitments[i].Member == centroids[a].name) {
-                        for (var b = 0; b < types.length; b++) {
-                            if (types[b].name == commitments[i].Type) {
-                                svg.append("rect")
-                                .attr("x", centroids[a].coordinates[0] - 25)
-                                .attr("y", centroids[a].coordinates[1] - 18)
-                                .attr("width", 50)
-                                .attr("height", 36)
-                                .attr("fill", types[b].color)
-                                .attr("class", "flagImage")
-                                .attr("class", "fadeInFlag");
-                            }
-                        }
+//                 // append the flag images to the map
+//                 for (var a = 0; a < centroids.length; a++) {
+//                     if (commitments[i].Member == centroids[a].name) {
+//                         for (var b = 0; b < types.length; b++) {
+//                             if (types[b].name == commitments[i].Type) {
+//                                 svg.append("rect")
+//                                 .attr("x", centroids[a].coordinates[0] - 25)
+//                                 .attr("y", centroids[a].coordinates[1] - 18)
+//                                 .attr("width", 50)
+//                                 .attr("height", 36)
+//                                 .attr("fill", types[b].color)
+//                                 .attr("class", "flagImage")
+//                                 .attr("class", "fadeInFlag");
+//                             }
+//                         }
 
-                    svg.append("image")
-                        .attr("href", centroids[a].flag)
-                        .attr("x", centroids[a].coordinates[0] - 25.5)
-                        .attr("y", centroids[a].coordinates[1] - 18.5)
-                        .attr("width", 49)
-                        .attr("height", 35)
-                        .attr("id", centroids[a].name)
-                        .attr("class", "flagImage")
-                        .attr("class", "fadeInFlag");
-                    } 
-                }
-            }
-        }
-        year++;
-    } else {
-        mediaRecorder.stop();
-    }
-}, 5000, year);
+//                     svg.append("image")
+//                         .attr("href", centroids[a].flag)
+//                         .attr("x", centroids[a].coordinates[0] - 25.5)
+//                         .attr("y", centroids[a].coordinates[1] - 18.5)
+//                         .attr("width", 49)
+//                         .attr("height", 35)
+//                         .attr("id", centroids[a].name)
+//                         .attr("class", "flagImage")
+//                         .attr("class", "fadeInFlag");
+//                     } 
+//                 }
+//             }
+//         }
+//         year++;
+//     } else {
+//         // mediaRecorder.stop();
+//     }
+// }, 5000, year);
 
 
 // append svg to #map div
